d9dcb6148061bd03b5514b1b822b4b0177262b49
rdjara <162420392+rdjara@users.noreply.github.com>
2024-03-22 05:23:31 +0000

    modified rk4

--- spinning_compass_functions.jl
+++ spinning_compass_functions.jl
@@ -2,13 +2,14 @@
 # Remember to put documentations on all the functions here. - Roy 
 # (delete this message before submission)
 
-"""
+
+function EOM_compass(r, t, B, ω)
+    """
     EOM_compass(r, t, B, ω)
 
-Equations of motion for a spinning compass in a periodically changing magnetic field
-
-"""
-function EOM_compass(r, t, B, ω)
+    Equations of motion for a spinning compass in a periodically changing magnetic field
+    """
+    
     (x, v) = r
     dxdt = v
     dvdt = -B*cos(ω*t)*sin(x)

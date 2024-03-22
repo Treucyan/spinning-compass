44ac20c62ab6b8cb147ea4758ce4436464643f4b
rdjara <162420392+rdjara@users.noreply.github.com>
2024-03-22 05:16:22 +0000

    added function

--- spinning_compass_functions.jl
+++ spinning_compass_functions.jl
@@ -1,3 +1,16 @@
 # Let's put all of our codes here - Roy 
 # Remember to put documentations on all the functions here. - Roy 
 # (delete this message before submission)
+
+"""
+    EOM_compass(r, t, B, ω)
+
+Equations of motion for a spinning compass in a periodically changing magnetic field
+
+"""
+function EOM_compass(r, t, B, ω)
+    (x, v) = r
+    dxdt = v
+    dvdt = -B*cos(ω*t)*sin(x)
+    return [dxdt, dvdt]
+end    

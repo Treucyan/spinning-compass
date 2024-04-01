9d28882601443ffb7de90dd576e0435f6c61e1c2
rdjara <jararoyjrd@gmail.com>
2024-04-01 09:48:37 +0800

    Changes: 1. Fixed the format of the documentation.
    2. Replaced the condition of time_param under the RK4 function from Array to Tuple{Float64, Float64, Int64}

--- spinning_compass_functions.jl
+++ spinning_compass_functions.jl
@@ -3,22 +3,29 @@
 # (delete this message before submission)
 
 
+
+#module for the equations of motions and rk4 algorithm
+module Spin_compass
+
+
+#equations of motions of the spinning compass
+"
+EOM_compass(r, t, B, ω)
+
+#Description
+Equations of motion for a spinning compass in a periodically changing magnetic field
+
+# Args
+    r (Array): Array containing the compass' state at time t. `r` must have the form: `r = [x, v]`
+    t (Float64): time t.
+    B (Float64): non-dimensional magnetic field amplitude
+    ω (Float64): non-dimensional driving frequency
+
+# Returns
+    [dxdt, dvdt] (Array): system's velocity state at t.
+"
 function EOM_compass(r::Array, t::Float64, B::Float64, ω::Float64)
-    """
-    EOM_compass(r, t, B, ω)
-
-    #Description
-    Equations of motion for a spinning compass in a periodically changing magnetic field
-
-    # Args
-        r (Array): Array containing the compass' state at time t. `r` must have the form: `r = [x, v]`
-        t (Float64): time t.
-        B (Float64): non-dimensional magnetic field amplitude
-        ω (Float64): driving frequency
-
-    # Returns
-        [dxdt, dvdt] (Array): system's velocity state at t.
-    """
+    
     
     (x, v) = r
     dxdt = v
@@ -30,26 +37,29 @@ end
 
 # general Runge Kutta algorithm 
 # (Sugested Edit: generalize (xpoints, ypoints) to make the function applicable for multidimensional systems)
-function RK4(eom_func::Function, time_param::Array, r::Array)
-    """
-    RK4(f, tpoints, r)
 
-    # Description
-    Solve a system of two first-order ODEs using the fourth-order Runge-Kutta method.
 
-    ## Args 
-        eom_func (Function): function corresponding to the equations of motion to be integrated.
-            The function must follow the format: `eom_func(r, t)`.
+"
+RK4(f, time_param, r)
 
-        time_param (Array): initial and final time, and number of steps of the integration. `time_param`
-            must follow the format: `time_param = [t_initial, t_final, Npoints]`
+# Description
+Solve a system of two first-order ODEs using the fourth-order Runge-Kutta method.
 
-        r (Array): initial state of the system. r must follow the same ordering of variables used in `eom_func`.
+## Args 
+    eom_func (Function): function corresponding to the equations of motion to be integrated.
+        The function must follow the format: `eom_func(r, t)`.
 
-    ## Returns
-        (xpoints, ypoints): Arrays containing the IVP solution of the EOM
+    time_param (Tuple): initial and final time, and number of steps of the integration. `time_param`
+        must follow the format: `time_param = [t_initial, t_final, Npoints]`
 
-    """
+    r (Array): initial state of the system. r must follow the same ordering of variables used in `eom_func`.
+
+## Returns
+    (xpoints, ypoints): Arrays containing the IVP solution of the EOM
+
+"
+function RK4(eom_func::Function, time_param::Tuple{Float64, Float64, Int64}, r::Array)
+    
     #initializing time parameters
     (t_initial, t_final, Npoints) = time_param
     h = abs(t_final - t_initial) / Npoints
@@ -70,5 +80,18 @@ function RK4(eom_func::Function, time_param::Array, r::Array)
         k4 = h*eom_func(r .+ k3, t + h)
         r = r .+ (k1 + 2*k2 + 2*k3 + k4) / 6
     end
-    return xpoints, ypoints
+    return tpoints, xpoints, ypoints
+end
+
+
+"
+    module_initializer()
+    
+    # Descrition
+    Prints Hello World!
+"
+function module_initializer()
+    println("Hello World")
+end
+
 end
